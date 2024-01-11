98ea3c2d1fd1dd26892f12bc60900f5ebd9cef69
Aniket Agrawal <aniket.mpa@gmail.com>
2024-01-11 07:15:40 +0100

    Remove data from index.js

--- index.js
+++ index.js
@@ -16,29 +16,6 @@ morgan.token("postContent", function (req, res) {
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :postContent"));
 
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
 app.get("/api/persons", (req, res) => {
     Entry.find({}).then(result => res.json(result));
 })
